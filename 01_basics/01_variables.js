f975c4675d1cbf153bba5ad2bc3ae39ebee5b80b
ankitgope1 <80397932+ankitgope1@users.noreply.github.com>
2024-06-17 15:40:05 +0000

    2

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "ankit@gmail.com"
+var accountPassword = "12345"
+accountCity = "Bangalore"
+let accountState;
+
+// accountId = 2 (not allowed)
+
+console.log(accountId);
+
+accountEmail= "abcd@con"
+accountPassword="abcde"
+accountCity="Jamshedpur"
+
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// alert (3+3) // we are using nodejs, not browser
+
+console.log(3+3);
+console.log("ankit");
+
+let name = "Ankit"
+let age = 29
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigInt 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
