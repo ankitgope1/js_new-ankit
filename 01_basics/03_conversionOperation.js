56b992a9b6c6786f943c4d407cd94dbc313dae34
ankitgope1 <80397932+ankitgope1@users.noreply.github.com>
2024-06-17 17:40:46 +0000

    ConversionOperation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -26,6 +26,42 @@ false => 0
 
 let someNumber = 33
 let someString = String(someNumber)
-console.table([someNumber,someString])
-console.log(typeof someString)
+// console.table([someNumber,someString])
+// console.log(typeof someString)
 
+
+
+// ********************************************* Operations *************************************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = 'Hello ';
+let str2 = 'World';
+let str3 = str1+str2;
+// console.log(str3);
+
+// console.log((3+4)*5%3);
+// console.log("1"+2);
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log(+true);
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2; 
+// console.log(num1)
+
+let gameCounter = 100
+gameCounter++;
+--gameCounter;
+console.log(gameCounter);
