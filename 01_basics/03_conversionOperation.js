7f96b3fd78b2f126a71ae4ba42bdc891726a4a4d
ankitgope1 <80397932+ankitgope1@users.noreply.github.com>
2024-06-17 16:03:39 +0000

    conversion datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = "abc"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0;
+
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+/* 
+true => 1,
+false => 0
+"" => 0
+"Word" => 1
+*/
+
+let someNumber = 33
+let someString = String(someNumber)
+console.table([someNumber,someString])
+console.log(typeof someString)
+
